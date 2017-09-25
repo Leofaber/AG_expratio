97f90306a45a61afa5d91f5b34ef1949611b7e8b
Leonardo Baroncelli <leonardo.baroncelli26@gmail.com>
2017-09-25 12:21:32 +0200

    little bug fix

--- src/Main.cpp
+++ src/Main.cpp
@@ -29,30 +29,30 @@ int main(int argc, char *argv[])
 { 	
 
 	cout << startString << endl;
-
-	if( argc > 6) 
-	{
+	
+	if( argc ==  7) 
+	{	
         const char * imagePath = argv[1];
         const char * outfile = argv[2];
-		double minThreshold = atof(argv[3]);
-		double maxThreshold = atof(argv[4]);
-		double l = atof(argv[5]);
-		double b = atof(argv[6]);
+	double minThreshold = (double) atof(argv[3]);
+	double maxThreshold = (double)atof(argv[4]);
+	double l = atof(argv[5]);
+	double b = atof(argv[6]);
 
-	   	ofstream resText(outfile);
-	   	resText.setf(ios::fixed); 
+   	ofstream resText(outfile);
+	resText.setf(ios::fixed); 
 
-		ExpRatioEvaluator exp(imagePath,minThreshold,maxThreshold,l,b);
-		double *output = exp.computeExpRatioValues();
+	ExpRatioEvaluator exp(imagePath,minThreshold,maxThreshold,l,b);
+	double *output = exp.computeExpRatioValues();
 
-		resText << setprecision(5) << output[0] <<" "<< output[1] <<" "<< output[2] <<" "<< output[3];
+	resText << setprecision(5) << output[0] <<" "<< output[1] <<" "<< output[2] <<" "<< output[3];
 	
-		cout << "Created " <<outfile<< " log file."<<endl;
-		resText.close();
+	cout << "Created " <<outfile<< " log file."<<endl;
+	resText.close();
 	}
 	else
 	{
-		printf("Expected 7 parameters:\n   - The .exp file path\n   - The name of the output file\n   - The minThreshold\n   - The maxThreshold\n   - The l coordinate\n    -The b coordinate");
+		printf("Expected 6 parameters:\n   - The .exp file path\n   - The name of the output file\n   - The minThreshold\n   - The maxThreshold\n   - The l coordinate\n    -The b coordinate\n");
 	}
 	
 	cout << endString << endl;
