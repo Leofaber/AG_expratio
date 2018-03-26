0ceaee8fb776c3ee7ae3d6a3368551904129dc2c
Giancarlo Zollino <giancarlo.zollino@gmail.com>
2018-03-26 10:53:27 +0200

    code improvement

--- run.sh
+++ run.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 module load agile-B24-r5
 export PFILES=/home/student/PARFILES
-cp ../ExpRatioEvaluator/include/ExpRatioEvaluator.h include/ExpRatioEvaluator.h
-cp ../ExpRatioEvaluator/src/ExpRatioEvaluator.cpp src/ExpRatioEvaluator.cpp
 
+# ExpRatioEvaluator
+cp ../AG_utils/include/ExpRatioEvaluator.h include/ExpRatioEvaluator.h
+cp ../AG_utils/src/ExpRatioEvaluator.cpp src/ExpRatioEvaluator.cpp
+
+# MapConverter
+cp ../AG_utils/include/MapConverter.h include/MapConverter.h
+cp ../AG_utils/src/MapConverter.cpp src/MapConverter.cpp
+
+
+make clean
 make
 
 
-./bin/AG_expratio log.txt exp_120_140_20_exp_norm.exp.gz no 45 30
+#./bin/AG_expratio logg.txt map/exp.exp no 45 30 no no -1 -1 -1 
+./bin/AG_expratio log.txt map/mappa.exp no 45 30
--- src/Main.cpp
+++ src/Main.cpp
@@ -6,13 +6,13 @@
  * Any information contained in this software
  * is property of the AGILE TEAM and is strictly
  * private and confidential.
- * 
+ *
  * https://github.com/Leofaber/AG_expratio
 */
 
 
 #include <fstream>
-#include <iomanip> 
+#include <iomanip>
 #include <cstring>
 #include <PilParams.h>
 
@@ -34,12 +34,12 @@ const PilDescription paramsDescr[] = {
 	{ PilString, "imagePath", "Input expMap file name"},
 	{ PilBool, "isExpMapNormalized","If 'yes' (or 'y') you assert that the exp-map is already normalized. Insert 'None' (or 'no') instead"},
 	{ PilReal, "l", "Longitude of map center (galactic)" },
-    	{ PilReal, "b", "Latitude of map center (galactic)" },        
-    	{ PilBool, "createExpNormalizedMap", "If 'yes' (or 'y') the normalized exp map will be written on file"},
-    	{ PilBool, "createExpRatioMap", "If true the exp-ratio map will be written on file"},
-    	{ PilReal, "minThreshold", "The lower bound for the threshold level in exp-ratio evaluation"},
-    	{ PilReal, "maxThreshold", "The upper bound for the threshold level in exp-ratio evaluation"},
-    	{ PilReal, "squareSize", "The degree dimension of the exp ratio evaluation area's edge"},
+	{ PilReal, "b", "Latitude of map center (galactic)" },
+	{ PilBool, "createExpNormalizedMap", "If 'yes' (or 'y') the normalized exp map will be written on file"},
+	{ PilBool, "createExpRatioMap", "If true the exp-ratio map will be written on file"},
+	{ PilReal, "minThreshold", "The lower bound for the threshold level in exp-ratio evaluation"},
+	{ PilReal, "maxThreshold", "The upper bound for the threshold level in exp-ratio evaluation"},
+	{ PilReal, "squareSize", "The degree dimension of the exp ratio evaluation area's edge"},
    	{ PilNone, "", "" }
 };
 
@@ -54,28 +54,28 @@ void printMatrix(double ** matrix, int rows, int cols){
 
 
 int main(int argc, char *argv[])
-{ 	
+{
 
 	cout << startString << endl;
-	
-	
+
+
 	PilParams params(paramsDescr);
    	if (!params.Load(argc, argv))
         	return EXIT_FAILURE;
-        
-        
-    	// PARAMETRI OBBLIGATORI ---------------------------------------------------
-    	const char *outfile		= params["outfile"];
-    	const char *imagePath 		= params["imagePath"];	
-    	bool isExpMapNormalized 	= params["isExpMapNormalized"];	
-	double l 			= params["l"];	
-	double b 			= params["b"];	
+
+
+	// PARAMETRI OBBLIGATORI ---------------------------------------------------
+	const char *outfile		= params["outfile"];
+	const char *imagePath 		= params["imagePath"];
+	bool isExpMapNormalized 	= params["isExpMapNormalized"];
+	double l 			= params["l"];
+	double b 			= params["b"];
 	bool createExpNormalizedMap 	= params["createExpNormalizedMap"];
 	bool createExpRatioMap 		= params["createExpRatioMap"];
-	double minThreshold 		= params["minThreshold"]; 
-	double maxThreshold 		= params["maxThreshold"]; 
-	double squareSize 		= params["squareSize"];  
-	 
+	double minThreshold 		= params["minThreshold"];
+	double maxThreshold 		= params["maxThreshold"];
+	double squareSize 		= params["squareSize"];
+
 
 	// PRINT INPUT PARAMETERS -------------------------------------
 
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
 	cout << "MinThreshold: " 		<< minThreshold << endl;
 	cout << "MaxThreshold: "		<< maxThreshold << endl;
 	cout << "squareSize: " 			<< squareSize << "\n" <<endl;
-	
+
 
 
 
@@ -98,31 +98,29 @@ int main(int argc, char *argv[])
 
 
 	ExpRatioEvaluator exp(	imagePath,
-				isExpMapNormalized,
-				createExpNormalizedMap, 
-				createExpRatioMap,
-				minThreshold, 
-				maxThreshold,
-				squareSize);
-				
+							isExpMapNormalized,
+							createExpNormalizedMap,
+							createExpRatioMap,
+							minThreshold,
+							maxThreshold,
+							squareSize);
+
 	double expRatio = exp.computeExpRatioValues(l,b);
 
 
 	// OUTPUT ----------------------------------------------------
-	
- 
-	ofstream resText(outfile);
-	resText.setf(ios::fixed); 
 
-	resText << setprecision(5) << expRatio; 
-	
+
+	ofstream resText(outfile);
+	resText.setf(ios::fixed);
+
+	resText << setprecision(5) << expRatio;
+
 	cout << "Created " << outfile << " log file."<<endl;
 	resText.close();
-	
+
 	cout << endString << endl;
 	return 0;
 
-	
+
 }
-		
-		
